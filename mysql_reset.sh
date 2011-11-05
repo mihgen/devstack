b64c095fab8eb6bba3da68dcf06f9a96121a8bd6
Mike Scherbakov <mihgen@gmail.com>
2011-11-04 22:45:08 -0700

    Set default mysql pass to nova. During the installation with stack.sh you have to specify it as a password for mysql.

--- mysql_reset.sh
+++ mysql_reset.sh
@@ -2,7 +2,7 @@
 /etc/init.d/mysql stop
 mysqld_safe --skip-grant-tables &
 sleep 1
-mysql -e 'use mysql; update user set password=PASSWORD("") where User="root"; flush privileges;'
+mysql -e 'use mysql; update user set password=PASSWORD("nova") where User="root"; flush privileges;'
 /etc/init.d/mysql stop
 killall mysqld
 killall mysqld_safe
